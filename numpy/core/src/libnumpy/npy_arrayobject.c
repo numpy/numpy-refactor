bcffa851025d600f279b0c1445e48fd69bd77b3d
Scott Swarts <swarts@enthought.com>
2010-05-24 11:27:40 -0500

    Finally really added changes to setup.py and new library (I hope).

--- numpy/core/src/libnumpy/npy_arrayobject.c
+++ numpy/core/src/libnumpy/npy_arrayobject.c
@@ -13,19 +13,3 @@ NpyArray_Size(NpyArray *op)
     return NpyArray_SIZE(op);
 }
 
-int
-NpyArray_TypeNumFromName(char *str)
-{
-    int i;
-    NpyArray_Descr *descr;
-
-    for (i = 0; i < NPY_NUMUSERTYPES; i++) {
-        descr = userdescrs[i];
-        /* XXX: We are looking at the python type for the name. This 
-           will need to be fixed. */
-        if (strcmp(descr->typeobj->tp_name, str) == 0) {
-            return descr->type_num;
-        }
-    }
-    return PyArray_NOTYPE;
-}
