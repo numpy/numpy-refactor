c3de990a2e56cd33a76536bff3361501d3aff14d
andy wilson <wilson.andrew.j+github@gmail.com>
2010-07-03 19:00:18 -0500

    adding more unit tests

--- numpy/core/tests/test_datetime.py
+++ numpy/core/tests/test_datetime.py
@@ -8,7 +8,7 @@ class TestDateTime(TestCase):
         for unit in ['Y', 'M', 'W', 'B', 'D',
                      'h', 'm', 's', 'ms', 'us',
                      'ns', 'ps', 'fs', 'as']:
-            dt1 = np.dtype('M8[750%s]'%unit)
+            dt1 = np.dtype('M8[750%s]' % unit)
             assert dt1 == np.dtype('datetime64[750%s]' % unit)
             dt2 = np.dtype('m8[%s]' % unit)
             assert dt2 == np.dtype('timedelta64[%s]' % unit)
@@ -72,7 +72,6 @@ class TestDateTime(TestCase):
 
 
 class TestDateTimeModulo(TestCase):
-    # TODO: Add checks for B, ms, us, ns, ps, fs, as
     def test_modulo_years(self):
         timesteps = np.array([0,1,2], dtype='datetime64[Y]//10')
         assert timesteps[0] == np.datetime64('1970')
@@ -84,7 +83,19 @@ class TestDateTimeModulo(TestCase):
         assert timesteps[0] == np.datetime64('1970-01')
         assert timesteps[1] == np.datetime64('1970-11')
         assert timesteps[2] == np.datetime64('1971-09')
+
+    def test_modulo_weeks(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[W]//3')
+        assert timesteps[0] == np.datetime64('1970-01-01')
+        assert timesteps[1] == np.datetime64('1970-01-22')
+        assert timesteps[2] == np.datetime64('1971-02-12')
         
+    def test_modulo_business_days(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[B]//4')
+        assert timesteps[0] == np.datetime64('1970-01-01')
+        assert timesteps[1] == np.datetime64('1970-01-07')
+        assert timesteps[2] == np.datetime64('1971-01-13')
+
     def test_modulo_days(self):
         timesteps = np.array([0,1,2], dtype='datetime64[D]//17')
         assert timesteps[0] == np.datetime64('1970-01-01')
@@ -108,6 +119,42 @@ class TestDateTimeModulo(TestCase):
         assert timesteps[1] == np.datetime64('1970-01-01 00:00:00')
         assert timesteps[1] == np.datetime64('1970-01-01 00:00:42')
         assert timesteps[1] == np.datetime64('1970-01-01 00:01:24')
+
+    def test_modulo_milliseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[ms]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.084')
+
+    def test_modulo_microseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[us]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.000084')
+
+    def test_modulo_nanoseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[ns]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.000000084')
+
+    def test_modulo_picoseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[ps]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.000000000084')
+
+    def test_modulo_femtoseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[fs]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000000000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000000042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.000000000000084')
+
+    def test_modulo_attoseconds(self):
+        timesteps = np.array([0,1,2], dtype='datetime64[as]//42')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000000000000')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:00:00.000000000000000042')
+        assert timesteps[1] == np.datetime64('1970-01-01 00:01:00.000000000000000084')
 
 
 class TestTimeDeltaSetters(TestCase):
@@ -173,6 +220,161 @@ class TestTimeDeltaComparisons(TestCase):
     def test_compare_timedelta64_to_string_broadcast(self):
         comparison = (self.timedeltas == '0:00:00.012')
         assert_equal(comparison, [True, False, True])
+
+
+class TestDateTimeAstype(TestCase):
+    def test_datetime_astype_years(self):
+        datetimes = np.array([0, 40, 15], dtype="datetime64[M]")
+        assert_equal(datetimes.astype('datetime64[Y]'), np.array([0, 3, 2], dtype="datetime64[Y]"))
+
+    def test_datetime_astype_months(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[Y]")
+        assert_equal(datetimes.astype('datetime64[M]'), np.array([0, 36, 24], dtype="datetime64[M]"))
+        datetimes = np.array([0, 100, 70], dtype="datetime64[D]")
+        assert_equal(datetimes.astype('datetime64[M]'), np.array([0, 3, 2], dtype="datetime64[M]"))
+
+    def test_datetime_astype_weeks(self):
+        datetimes = np.array([0, 22, 15], dtype="datetime64[D]")
+        assert_equal(datetimes.astype('datetime64[W]'), np.array([0, 3, 2], dtype="datetime64[W]"))
+
+    def test_datetime_astype_business_days(self):
+        # XXX: There will probably be a more direct way to check for
+        #      *Not a Time* values.
+        datetimes = np.arange(5, dtype='datetime64[D]')
+        expected_array_str = '[1970-01-01  1970-01-02  NaT  NaT  1970-01-05]'
+        assert_equal(datetimes.astype('datetime64[B]'), expected_array_str)
+
+    def test_datetime_astype_days(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[W]")
+        assert_equal(datetimes.astype('datetime64[D]'), np.array([0, 21, 7], dtype="datetime64[D]"))
+        datetimes = np.array([0, 37, 24], dtype="datetime64[h]")
+        assert_equal(datetimes.astype('datetime64[D]'), np.array([0, 3, 2], dtype="datetime64[D]"))
+
+    def test_datetime_astype_hours(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[D]")
+        assert_equal(datetimes.astype('datetime64[h]'), np.array([0, 36, 24], dtype="datetime64[D]"))
+        datetimes = np.array([0, 190, 153], dtype="datetime64[m]")
+        assert_equal(datetimes.astype('datetime64[h]'), np.array([0, 3, 2], dtype="datetime64[h]"))
+
+    def test_datetime_astype_minutes(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[h]")
+        assert_equal(datetimes.astype('datetime64[m]'), np.array([0, 180, 120], dtype="datetime64[m]"))
+        datetimes = np.array([0, 190, 153], dtype="datetime64[s]")
+        assert_equal(datetimes.astype('datetime64[m]'), np.array([0, 3, 2], dtype="datetime64[m]"))
+
+    def test_datetime_astype_seconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[m]")
+        assert_equal(datetimes.astype('datetime64[s]'), np.array([0, 180, 120], dtype="datetime64[s]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[ms]")
+        assert_equal(datetimes.astype('datetime64[s]'), np.array([0, 3, 2], dtype="datetime64[s]"))
+
+    def test_datetime_astype_milliseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[s]")
+        assert_equal(datetimes.astype('datetime64[ms]'), np.array([0, 3000, 2000], dtype="datetime64[ms]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[us]")
+        assert_equal(datetimes.astype('datetime64[ms]'), np.array([0, 3, 2], dtype="datetime64[ms]"))
+
+    def test_datetime_astype_microseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[ms]")
+        assert_equal(datetimes.astype('datetime64[us]'), np.array([0, 3000, 2000], dtype="datetime64[us]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[ns]")
+        assert_equal(datetimes.astype('datetime64[us]'), np.array([0, 3, 2], dtype="datetime64[us]"))
+
+    def test_datetime_astype_nanoseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[us]")
+        assert_equal(datetimes.astype('datetime64[ns]'), np.array([0, 3000, 2000], dtype="datetime64[ns]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[ps]")
+        assert_equal(datetimes.astype('datetime64[ns]'), np.array([0, 3, 2], dtype="datetime64[ns]"))
+
+    def test_datetime_astype_picoseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[ns]")
+        assert_equal(datetimes.astype('datetime64[ps]'), np.array([0, 3000, 2000], dtype="datetime64[ps]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[ns]")
+        assert_equal(datetimes.astype('datetime64[ps]'), np.array([0, 3, 2], dtype="datetime64[ps]"))
+
+    def test_datetime_astype_femtoseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[ps]")
+        assert_equal(datetimes.astype('datetime64[fs]'), np.array([0, 3000, 2000], dtype="datetime64[fs]"))
+        datetimes = np.array([0, 3200, 2430], dtype="datetime64[ps]")
+        assert_equal(datetimes.astype('datetime64[fs]'), np.array([0, 3, 2], dtype="datetime64[fs]"))
+
+    def test_datetime_astype_attoseconds(self):
+        datetimes = np.array([0, 3, 2], dtype="datetime64[fs]")
+        assert_equal(datetimes.astype('datetime64[as]'), np.array([0, 3000, 2000], dtype="datetime64[as]"))
+
+
+class TestTimeDeltaAstype(TestCase):
+    def test_timedelta_astype_years(self):
+        timedeltas = np.array([0, 40, 15], dtype="timedelta64[M]")
+        assert_equal(timedeltas.astype('timedelta64[Y]'), np.array([0, 3, 2], dtype="timedelta64[Y]"))
+
+    def test_timedelta_astype_months(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[Y]")
+        assert_equal(timedeltas.astype('timedelta64[M]'), np.array([0, 36, 24], dtype="timedelta64[M]"))
+        timedeltas = np.array([0, 100, 70], dtype="timedelta64[D]")
+        assert_equal(timedeltas.astype('timedelta64[M]'), np.array([0, 3, 2], dtype="timedelta64[M]"))
+
+    def test_timedelta_astype_weeks(self):
+        timedeltas = np.array([0, 22, 15], dtype="timedelta64[D]")
+        assert_equal(timedeltas.astype('timedelta64[W]'), np.array([0, 3, 2], dtype="timedelta64[W]"))
+
+    def test_timedelta_astype_days(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[W]")
+        assert_equal(timedeltas.astype('timedelta64[D]'), np.array([0, 21, 7], dtype="timedelta64[D]"))
+        timedeltas = np.array([0, 37, 24], dtype="timedelta64[h]")
+        assert_equal(timedeltas.astype('timedelta64[D]'), np.array([0, 3, 2], dtype="timedelta64[D]"))
+
+    def test_timedelta_astype_hours(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[D]")
+        assert_equal(timedeltas.astype('timedelta64[h]'), np.array([0, 36, 24], dtype="timedelta64[D]"))
+        timedeltas = np.array([0, 190, 153], dtype="timedelta64[m]")
+        assert_equal(timedeltas.astype('timedelta64[h]'), np.array([0, 3, 2], dtype="timedelta64[h]"))
+
+    def test_timedelta_astype_minutes(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[h]")
+        assert_equal(timedeltas.astype('timedelta64[m]'), np.array([0, 180, 120], dtype="timedelta64[m]"))
+        timedeltas = np.array([0, 190, 153], dtype="timedelta64[s]")
+        assert_equal(timedeltas.astype('timedelta64[m]'), np.array([0, 3, 2], dtype="timedelta64[m]"))
+
+    def test_timedelta_astype_seconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[m]")
+        assert_equal(timedeltas.astype('timedelta64[s]'), np.array([0, 180, 120], dtype="timedelta64[s]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[ms]")
+        assert_equal(timedeltas.astype('timedelta64[s]'), np.array([0, 3, 2], dtype="timedelta64[s]"))
+
+    def test_timedelta_astype_milliseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[s]")
+        assert_equal(timedeltas.astype('timedelta64[ms]'), np.array([0, 3000, 2000], dtype="timedelta64[ms]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[us]")
+        assert_equal(timedeltas.astype('timedelta64[ms]'), np.array([0, 3, 2], dtype="timedelta64[ms]"))
+
+    def test_timedelta_astype_microseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[ms]")
+        assert_equal(timedeltas.astype('timedelta64[us]'), np.array([0, 3000, 2000], dtype="timedelta64[us]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[ns]")
+        assert_equal(timedeltas.astype('timedelta64[us]'), np.array([0, 3, 2], dtype="timedelta64[us]"))
+
+    def test_timedelta_astype_nanoseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[us]")
+        assert_equal(timedeltas.astype('timedelta64[ns]'), np.array([0, 3000, 2000], dtype="timedelta64[ns]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[ps]")
+        assert_equal(timedeltas.astype('timedelta64[ns]'), np.array([0, 3, 2], dtype="timedelta64[ns]"))
+
+    def test_timedelta_astype_picoseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[ns]")
+        assert_equal(timedeltas.astype('timedelta64[ps]'), np.array([0, 3000, 2000], dtype="timedelta64[ps]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[ns]")
+        assert_equal(timedeltas.astype('timedelta64[ps]'), np.array([0, 3, 2], dtype="timedelta64[ps]"))
+
+    def test_timedelta_astype_femtoseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[ps]")
+        assert_equal(timedeltas.astype('timedelta64[fs]'), np.array([0, 3000, 2000], dtype="timedelta64[fs]"))
+        timedeltas = np.array([0, 3200, 2430], dtype="timedelta64[ps]")
+        assert_equal(timedeltas.astype('timedelta64[fs]'), np.array([0, 3, 2], dtype="timedelta64[fs]"))
+
+    def test_timedelta_astype_attoseconds(self):
+        timedeltas = np.array([0, 3, 2], dtype="timedelta64[fs]")
+        assert_equal(timedeltas.astype('timedelta64[as]'), np.array([0, 3000, 2000], dtype="datetime64[as]"))
 
 
 if __name__ == "__main__":
