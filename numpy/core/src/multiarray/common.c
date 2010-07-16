22ff78666cc8b5bc6bbf1c051c9a6e3af116fbba
Scott Swarts <swarts@enthought.com>
2010-07-16 12:54:32 -0500

    Made _array_small_type just call NpyArray_SmallType.

--- numpy/core/src/multiarray/common.c
+++ numpy/core/src/multiarray/common.c
@@ -22,67 +22,8 @@
 NPY_NO_EXPORT PyArray_Descr *
 _array_small_type(PyArray_Descr *chktype, PyArray_Descr* mintype)
 {
-    PyArray_Descr *outtype;
-    int outtype_num, save_num;
-
-    if (PyArray_EquivTypes(chktype, mintype)) {
-        Py_INCREF(mintype);
-        return mintype;
-    }
-
-
-    if (chktype->type_num > mintype->type_num) {
-        outtype_num = chktype->type_num;
-    }
-    else {
-        if (PyDataType_ISOBJECT(chktype) &&
-            PyDataType_ISSTRING(mintype)) {
-            return PyArray_DescrFromType(NPY_OBJECT);
-        }
-        else {
-            outtype_num = mintype->type_num;
-        }
-    }
-
-    save_num = outtype_num;
-    while (outtype_num < PyArray_NTYPES &&
-          !(PyArray_CanCastSafely(chktype->type_num, outtype_num)
-            && PyArray_CanCastSafely(mintype->type_num, outtype_num))) {
-        outtype_num++;
-    }
-    if (outtype_num == PyArray_NTYPES) {
-        outtype = PyArray_DescrFromType(save_num);
-    }
-    else {
-        outtype = PyArray_DescrFromType(outtype_num);
-    }
-    if (NpyTypeNum_ISEXTENDED(outtype->type_num)) {
-        int testsize = outtype->elsize;
-        int chksize, minsize;
-        chksize = chktype->elsize;
-        minsize = mintype->elsize;
-        /*
-         * Handle string->unicode case separately
-         * because string itemsize is 4* as large
-         */
-        if (outtype->type_num == PyArray_UNICODE &&
-            mintype->type_num == PyArray_STRING) {
-            testsize = MAX(chksize, 4*minsize);
-        }
-        else if (chktype->type_num == PyArray_STRING &&
-                 mintype->type_num == PyArray_UNICODE) {
-            testsize = MAX(chksize*4, minsize);
-        }
-        else {
-            testsize = MAX(chksize, minsize);
-        }
-        if (testsize != outtype->elsize) {
-            PyArray_DESCR_REPLACE(outtype);
-            outtype->elsize = testsize;
-            NpyArray_DescrDeallocNamesAndFields(outtype);
-        }
-    }
-    return outtype;
+    /* TODO: Wrap and unwrap descriptors. */
+    return NpyArray_SmallType(chktype, mintype);
 }
 
 NPY_NO_EXPORT PyArray_Descr *
