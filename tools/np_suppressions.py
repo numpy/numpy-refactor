6c4a2d6f80d7ee5f9c06c3d678bb86661c94a793
Geoffrey Furnish <furnish@enthought.com>
2010-07-23 14:22:12 -0500

    Add documentation on one assertion, convert RE's to raw strings.

--- tools/np_suppressions.py
+++ tools/np_suppressions.py
@@ -1,11 +1,14 @@
 suppressions = [
-    [ ".*/multiarray/mapping\.", "PyArray_MapIterReset" ],
+    # This one cannot be covered by any Python language test because there is
+    # no code pathway to it.  But it is part of the C API, so must not be
+    # excised from the code.
+    [ r".*/multiarray/mapping\.", "PyArray_MapIterReset" ],
 
     # PyArray_Std trivially forwards to and appears to be superceded by
     # __New_PyArray_Std, which is exercised by the test framework.
-    [ ".*/multiarray/calculation\.", "PyArray_Std" ],
+    [ r".*/multiarray/calculation\.", "PyArray_Std" ],
 
     # PyCapsule_Check is declared in a header, and used in
     # multiarray/ctors.c.  So it isn't really untested.
-    [ ".*/multiarray/common\.", "PyCapsule_Check" ],
+    [ r".*/multiarray/common\.", "PyCapsule_Check" ],
     ]
