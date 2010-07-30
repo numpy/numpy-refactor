cf83f2b03cbba4527462e9138bcdf97c6e3466c2
Ilan Schnell <ischnell@enthought.com>
2010-07-30 14:50:20 -0500

    split array_strides_set

--- numpy/core/src/libnumpy/npy_getset.c
+++ numpy/core/src/libnumpy/npy_getset.c
@@ -53,3 +53,55 @@ NpyArray_SetShape(NpyArray *self, NpyArray_Dims *newdims)
     NpyArray_UpdateFlags(self, NPY_CONTIGUOUS | NPY_FORTRAN);
     return 0;
 }
+
+
+/*NUMPY_API
+ */
+int
+NpyArray_SetStrides(NpyArray *self, NpyArray_Dims *newstrides)
+{
+    NpyArray *new;
+    npy_intp numbytes = 0, offset = 0;
+    Py_ssize_t buf_len;
+    char *buf;
+
+    if (newstrides->len != NpyArray_NDIM(self)) {
+        NpyErr_SetString(NpyExc_ValueError,
+                         "strides must be same length as shape");
+        return -1;
+    }
+    new = NpyArray_BASE_ARRAY(self);
+    while(NULL != NpyArray_BASE_ARRAY(new)) {
+        new = NpyArray_BASE_ARRAY(new);
+    }
+
+    /* Get the available memory through the buffer interface on
+     * new->base or if that fails from the current new
+     */
+    if (new->base_obj != NULL && PyObject_AsReadBuffer(new->base_obj,
+                                                       (const void **)&buf,
+                                                       &buf_len) >= 0) {
+        offset = NpyArray_BYTES(self) - buf;
+        numbytes = buf_len - offset;
+    }
+    else {
+        PyErr_Clear();
+        numbytes = NpyArray_MultiplyList(
+                       NpyArray_DIMS(new),
+                       NpyArray_NDIM(new)) * NpyArray_ITEMSIZE(new);
+        offset = NpyArray_BYTES(self) - NpyArray_BYTES(new);
+    }
+
+    if (!NpyArray_CheckStrides(NpyArray_ITEMSIZE(self),
+                               NpyArray_NDIM(self), numbytes,
+                               offset,
+                               NpyArray_DIMS(self), newstrides->ptr)) {
+        NpyErr_SetString(NpyExc_ValueError,
+                         "strides is not compatible with available memory");
+        return -1;
+    }
+    memcpy(NpyArray_STRIDES(self), newstrides->ptr,
+           sizeof(npy_intp) * newstrides->len);
+    NpyArray_UpdateFlags(self, NPY_CONTIGUOUS | NPY_FORTRAN);
+    return 0;
+}
