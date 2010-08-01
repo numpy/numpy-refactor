6688e359656dc65d2d42ceb1fb397c8243a4a5af
Ilan Schnell <ilan@mira.local>
2010-07-31 19:15:53 -0500

    add experimental code for exception handling

--- numpy/core/src/libnumpy/npy_exceptions.c
+++ numpy/core/src/libnumpy/npy_exceptions.c
@@ -1,15 +1,83 @@
 /*
- *  npy_error.c -
+ *  npy_exceptions.c -
  *
  */
-
-#define _MULTIARRAYMODULE
-#define PY_SSIZE_T_CLEAN
-#include "npy_config.h"
-#include "numpy/numpy_api.h"
-
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 
-/*
-  TODO
-*/
+enum {
+    NpyExc_NOERROR,
+    NpyExc_ValueError,
+    NpyExc_MemoryError,
+    NpyExc_IOError,
+    NpyExc_TypeError,
+    NpyExc_IndexError,
+    NpyExc_RuntimeError,
+    NpyExc_AttributeError,
+};
+
+
+int _type = NpyExc_NOERROR;
+char _msg[256];
+
+
+void NpyErr_Clear()
+{
+    _type = NpyExc_NOERROR;
+}
+
+
+int NpyErr_Occurred()
+{
+    return _type == NpyExc_NOERROR ? 0 : 1;
+}
+
+
+void NpyErr_SetString(int exc, const char *str)
+{
+    _type = exc;
+    assert(strlen(str) < 256);
+    strcpy(_msg, str);
+}
+
+
+void NpyErr_Format(int exc, const char *format, ...)
+{
+    va_list vargs;
+
+    _type = exc;
+    va_start(vargs, format);
+    vsprintf(_msg, format, vargs);
+    va_end(vargs);
+}
+
+
+void NpyErr_NoMemory()
+{
+    _type = NpyExc_MemoryError;
+}
+
+
+void NpyErr_Print()
+{
+    assert(NpyErr_Occurred());
+    fprintf(stderr, "%d: %s\n", _type, _msg);
+}
+
+
+int main()
+{
+    NpyErr_SetString(NpyExc_TypeError, "something has wrong type");
+    NpyErr_Print();
+    NpyErr_Format(NpyExc_ValueError, "too large");
+    NpyErr_Print();
+    NpyErr_Format(NpyExc_ValueError, "too large (%d)", 32);
+    NpyErr_Print();
+    NpyErr_Format(NpyExc_ValueError, "too large (%d) %s", 32, "bad");
+    NpyErr_Print();
+
+    return 0;
+}
