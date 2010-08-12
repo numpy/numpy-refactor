86654737596d930f046c515588874693d6dc9747
Jason McCampbell <jmccampbell@enthought.com>
2010-08-12 11:38:36 -0500

    Added npy_number.c in prep for moving code in.

---
+++ libndarray/src/npy_number.c
@@ -0,0 +1,12 @@
+/*
+ *  npy_number.c -
+ *
+ */
+
+#define _MULTIARRAYMODULE
+#include <stdlib.h>
+#include <strings.h>
+#include "npy_config.h"
+#include "numpy_api.h"
+//#include "npy_ufunc_object.h"
+
