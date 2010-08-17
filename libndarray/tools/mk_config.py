6b65cd04beee4df360d97f0f7957fb648c4f4c31
Ilan Schnell <ischnell@enthought.com>
2010-08-17 13:41:42 -0500

    add comment

--- libndarray/tools/mk_config.py
+++ libndarray/tools/mk_config.py
@@ -70,7 +70,13 @@ def main():
 /* npy_config.h   Generated from config.h by tools/mk_config.py */
 ''' + rename_defs(data)
 
-    if sys.platform != 'darwin':
+    if sys.platform == 'darwin':
+        data += '''
+/* long double representation is not added by tools/mk_config.py on OSX
+   because NPY_LDOUBLE_??? is definied in src/npy_fpmath.h for OSX
+*/
+'''
+    else:
         ld_repr = check_long_double_repr()
         print "\tdetected long double representation:", ld_repr
         data += '''
