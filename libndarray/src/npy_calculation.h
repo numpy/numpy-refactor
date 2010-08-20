c13812c45c88f9867ca809934bcd5c6a31fe2580
Jason McCampbell <jmccampbell@enthought.com>
2010-08-20 17:45:01 -0500

    Missed npy_calculation.h

---
+++ libndarray/src/npy_calculation.h
@@ -0,0 +1,10 @@
+
+#if !defined(_NPY_CALCULATION_H_)
+#define _NPY_CALCULATION_H_
+
+struct NpyArray;
+
+struct NpyArray *NpyArray_Conjugate(struct NpyArray *self, struct NpyArray *out);
+
+
+#endif
