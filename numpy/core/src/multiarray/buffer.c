45a80c79792881e2b6534cfeaaa7b96dc7bb6c53
Ilan Schnell <ischnell@enthought.com>
2010-09-01 17:25:00 -0500

    fixed config variable name

--- numpy/core/src/multiarray/buffer.c
+++ numpy/core/src/multiarray/buffer.c
@@ -268,7 +268,7 @@ _buffer_format_string(NpyArray_Descr *descr, _tmp_string_t *str,
         int is_standard_size = 1;
         int is_native_only_type = (descr->type_num == NPY_LONGDOUBLE ||
                                    descr->type_num == NPY_CLONGDOUBLE);
-#if NPY_SIZEOF_LONG_LONG != 8
+#if NPY_SIZEOF_LONGLONG != 8
         is_native_only_type = is_native_only_type || (
             descr->type_num == NPY_LONGLONG ||
             descr->type_num == NPY_ULONGLONG);
