e8e84518552967889935934f42fd7884a92bf097
Ilan Schnell <ischnell@enthought.com>
2010-09-15 15:05:20 -0500

    adding install function

--- ironsetup.py
+++ ironsetup.py
@@ -1,6 +1,8 @@
 import os
 import sys
-from os.path import join
+import shutil
+import tempfile
+from os.path import isdir, isfile, join
 
 
 if sys.platform != 'cli':
@@ -16,5 +18,23 @@ def msbuild():
     os.chdir(src_dir)
 
 
+def install():
+    print "INSTALLING ..."
+    sp_dir = join(sys.prefix, r'Lib\site-packages')
+    bin_dir = join(src_dir, r'numpy\NumpyDotNet\bin\Debug')
+    dll_dir = join(sys.prefix, 'DLLs')
+    if not isdir(dll_dir):
+        os.mkdir(dll_dir)
+    shutil.copy(join(bin_dir, 'numpy.py'), sp_dir)
+    for fn in ['ndarray.dll', 'NpyAccessLib.dll', 'NumpyDotNet.dll']:
+        src = join(bin_dir, fn)
+        dst = join(dll_dir, fn)
+        if isfile(dst):
+            tmp_dir = tempfile.mkdtemp()
+            os.rename(dst, join(tmp_dir, fn))
+        shutil.copyfile(src, dst)
+
+
 if __name__ == '__main__':
     msbuild()
+    install()
