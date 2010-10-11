0909c99447195e9b1cde9161eaafa666a526222c
Scott Swarts <swarts@enthought.com>
2010-10-11 17:14:41 -0500

    Added exports of empty, zeros, arange, putmask and lexsort for .NET.

--- numpy/core/numeric.py
+++ numpy/core/numeric.py
@@ -28,8 +28,8 @@ __all__ = ['newaxis', 'ndarray', 'flatiter', 'ufunc',
 
 if sys.platform == 'cli':
     obj2sctype = isnan = zeros = None
-    for n in '''ufunc arange empty fromstring fromfile frombuffer int_asbuffer
-where concatenate fastCopyAndTranspose lexsort set_numeric_ops can_cast inner
+    for n in '''ufunc fromstring fromfile frombuffer int_asbuffer
+where concatenate fastCopyAndTranspose set_numeric_ops can_cast inner
 dot vdot alterdot restoredot array2string get_printoptions set_printoptions
 set_string_function fromiter compare_chararrays
 Inf inf infty Infinity nan NaN False_ True_
@@ -220,7 +220,12 @@ newaxis = None
 
 
 if sys.platform == 'cli':
+    arange = NDNMM.arange
     array = NDNMM.array
+    putmask = NDNMM.putmask
+    zeros = NDNMM.zeros
+    empty = NDNMM.empty
+    lexsort = NDNMM.lexsort
     putmask = NDNMM.putmask
 else:
     arange = multiarray.arange
