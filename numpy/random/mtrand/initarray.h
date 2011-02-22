d9378c70af0c0100d62095e33c9ac9bacde8903d
Jason McCampbell (Enthought, Inc) <jmccampbell@enthought.com>
2011-02-22 13:05:13 -0600

    Re-generated mtrand.cpp with latest Cython, fixed up reference paths.

--- numpy/random/mtrand/initarray.h
+++ numpy/random/mtrand/initarray.h
@@ -1,6 +1,13 @@
 #include "randomkit.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void
 init_by_array(rk_state *self, unsigned long init_key[],
               unsigned long key_length);
 
+#ifdef __cplusplus
+}
+#endif
